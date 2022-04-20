3943e6e305184c1a138a860b51d2e06af88cabe1
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-04-20 05:39:39 -0400

    fix input state change and add value

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ import Form from "./components/Form";
 import TodoList from './components/TodoList';
 function App() {
   const [inputText, setInputText] = useState("");
- return (
-   <div className='App'>
-     <h1>Alparslan's Todo List</h1>
-     <Form />
-     <TodoList/>
-   </div>
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className='App'>
+      <h1>Alparslan's Todo List</h1>
+      <Form setInputText={setInputText}
+        setTodos={setTodos}
+        inputText={inputText}
+        todos={todos} />
+      <TodoList/>
+    </div>
 
- )
+  )
 }
 
 export default App;
