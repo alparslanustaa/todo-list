545b9a8a28b5dc0e142bc248cdfaa98075136d83
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-04-20 04:47:03 -0400

    add components for form element

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
-
+import React from 'react';
+// importing comps
+import Form from "./components/Form";
 function App() {
  return (
    <div className='App'>
-     <h1>Hello React</h1>
+     <h1>Alparslan's Todo List</h1>
+     <Form />
    </div>
 
  )
