4035225a7ccf7dd89648645398595b3e3f391d37
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-04-21 06:23:34 -0400

    add todo list components

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,15 @@
-
-import React from "react"
+import Todo from "./Todo";
+import React from "react";
 
 const TodoList = () => {
   return (
     <div className="todo-container">
-      <ul className="todo-list"></ul>
+      <ul className="todo-list">
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+      </ul>
     </div>
   );
 }
