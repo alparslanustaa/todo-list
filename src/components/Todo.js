09eff47bf9a666666a5fa3212d8ec020881c21d0
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-04-22 03:32:47 -0400

    implement completeHendler button function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,26 @@
 import React from "react";
 
 
-const Todo = ({text, todo , todos, setTodos}) => {
+const Todo = ({ text, todo, todos, setTodos }) => {
   //EVENTS
   const deleteHandler = () => {
     setTodos(todos.filter(el => el.id !== todo.id));
   };
 
-  
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
   return (
     <div className="todo">
       <li className="todo-item">{text}</li>
