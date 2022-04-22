870be52d016550ff43529e63f4331080c3063f4f
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-04-22 03:42:41 -0400

    implement state change to css for completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -23,8 +23,8 @@ const Todo = ({ text, todo, todos, setTodos }) => {
 
   return (
     <div className="todo">
-      <li className="todo-item">{text}</li>
-      <button className="complete-btn">
+      <li className={ `todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
       <button onClick={deleteHandler} className="trash-btn">
